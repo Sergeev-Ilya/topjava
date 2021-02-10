a1a1aba4e88dd94a052ff5cbb3bca160736cc0ea
Ilya Sergeev <ilbras@mail.ru>
2021-02-10 13:05:31 +0300

    adding a bit of concurrency

--- src/main/java/ru/javawebinar/topjava/dao/MealDao.java
+++ src/main/java/ru/javawebinar/topjava/dao/MealDao.java
@@ -5,22 +5,23 @@ import ru.javawebinar.topjava.model.Meal;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static java.time.LocalDateTime.of;
 import static java.time.Month.JANUARY;
 
 public class MealDao implements Dao<Meal> {
-    private static int count = 0;
+    private static final AtomicInteger count = new AtomicInteger();
     private static final List<Meal> meals = new ArrayList<>();
 
     static {
-        meals.add(new Meal(++count, of(2020, JANUARY, 30, 10, 0), "Завтрак", 500));
-        meals.add(new Meal(++count, of(2020, JANUARY, 30, 13, 0), "Обед", 1000));
-        meals.add(new Meal(++count, of(2020, JANUARY, 30, 20, 0), "Ужин", 500));
-        meals.add(new Meal(++count, of(2020, JANUARY, 31, 0, 0), "Еда на граничное значение", 100));
-        meals.add(new Meal(++count, of(2020, JANUARY, 31, 10, 0), "Завтрак", 1000));
-        meals.add(new Meal(++count, of(2020, JANUARY, 31, 13, 0), "Обед", 500));
-        meals.add(new Meal(++count, of(2020, JANUARY, 31, 20, 0), "Ужин", 410));
+        meals.add(new Meal(count.incrementAndGet(), of(2020, JANUARY, 30, 10, 0), "Завтрак", 500));
+        meals.add(new Meal(count.incrementAndGet(), of(2020, JANUARY, 30, 13, 0), "Обед", 1000));
+        meals.add(new Meal(count.incrementAndGet(), of(2020, JANUARY, 30, 20, 0), "Ужин", 500));
+        meals.add(new Meal(count.incrementAndGet(), of(2020, JANUARY, 31, 0, 0), "Еда на граничное значение", 100));
+        meals.add(new Meal(count.incrementAndGet(), of(2020, JANUARY, 31, 10, 0), "Завтрак", 1000));
+        meals.add(new Meal(count.incrementAndGet(), of(2020, JANUARY, 31, 13, 0), "Обед", 500));
+        meals.add(new Meal(count.incrementAndGet(), of(2020, JANUARY, 31, 20, 0), "Ужин", 410));
     }
 
     public List<Meal> getAll() {
@@ -48,7 +49,7 @@ public class MealDao implements Dao<Meal> {
 
     @Override
     public void add(final Meal meal) {
-        meals.add(new Meal(++count, meal));
+        meals.add(new Meal(count.incrementAndGet(), meal));
     }
 
     private void setAllPropertiesFromTo(final Meal mealToUpdate, final Meal updatedMeal) {
