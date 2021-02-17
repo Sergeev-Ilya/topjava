136926a836473063b2b1dea376b866f39f0c28af
Ilya Sergeev <ilbras@mail.ru>
2021-02-17 20:49:19 +0300

    HW03 - added meals to sql scripts

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,6 +1,7 @@
 DELETE FROM user_roles;
+DELETE FROM user_meals;
 DELETE FROM users;
-ALTER SEQUENCE global_seq RESTART WITH 100000;
+DELETE FROM meals;
 
 INSERT INTO users (name, email, password)
 VALUES ('User', 'user@yandex.ru', 'password'),
@@ -9,3 +10,21 @@ VALUES ('User', 'user@yandex.ru', 'password'),
 INSERT INTO user_roles (role, user_id)
 VALUES ('USER', 100000),
        ('ADMIN', 100001);
+
+INSERT INTO meals (date_time, description, calories)
+VALUES  (TIMESTAMP '2021-02-11 11:30:00', 'Завтрак', 500),
+        (TIMESTAMP '2021-02-11 15:00:00', 'Обед', 1000),
+        (TIMESTAMP '2021-02-11 19:30:00', 'Ужин', 500),
+        (TIMESTAMP '2021-02-12 00:00:00', 'Еда на граничное значение', 100),
+        (TIMESTAMP '2021-02-12 11:30:00', 'Завтрак', 1000),
+        (TIMESTAMP '2021-02-12 15:00:00', 'Обед', 1000),
+        (TIMESTAMP '2021-02-12 19:30:00', 'Ужин', 410);
+
+INSERT INTO user_meals (meal_id, user_id)
+VALUES  (100002, 100000),
+        (100003, 100000),
+        (100004, 100000),
+        (100005, 100000),
+        (100006, 100000),
+        (100007, 100000),
+        (100008, 100000);
